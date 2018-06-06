e1a5fee2f1d45b3b9b75e382d35e770ff92be3fa
adriandavidbrand <39876791+adriandavidbrand@users.noreply.github.com>
2018-06-06 13:48:00 +1000

    Add persistence

--- projects/ngx-rxcache/src/lib/models/rxcache-item-config.ts
+++ projects/ngx-rxcache/src/lib/models/rxcache-item-config.ts
@@ -1,10 +1,12 @@
 import { Observable } from "rxjs";
 
 export interface RxCacheItemConfig<T> {
-    id: string;
-    construct?: () => Observable<T>;
-    load?: boolean;    
-    initialValue?: T;
-    genericError?: string;
-    errorHandler?: (id: string, error?: any) => string;
+  id: string;
+  construct?: () => Observable<T>;
+  persist?: (val: T) => Observable<any>;
+  saved?: (val: any) => void;
+  load?: boolean;
+  initialValue?: T;
+  genericError?: string;
+  errorHandler?: ( id: string, error?: any ) => string;
 }
