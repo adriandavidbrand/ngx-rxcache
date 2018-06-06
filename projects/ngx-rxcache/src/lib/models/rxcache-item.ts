734c8a7fed444dd75fd6d1102d195fe3e6ee94ae
adriandavidbrand <39876791+adriandavidbrand@users.noreply.github.com>
2018-06-06 13:47:24 +1000

    Add persistence

--- projects/ngx-rxcache/src/lib/models/rxcache-item.ts
+++ projects/ngx-rxcache/src/lib/models/rxcache-item.ts
@@ -1,14 +1,18 @@
 import { Observable, BehaviorSubject, Subscription } from "rxjs";
 
 export interface RxCacheItem<T> {
-    id: string;
-    instance$: BehaviorSubject<T>;
-    loaded$: BehaviorSubject<boolean>;
-    loading$: BehaviorSubject<boolean>;
-    hasError$: BehaviorSubject<boolean>;
-    error$: BehaviorSubject<string>;
-    genericError: string
-    construct?: () => Observable<T>;
-    errorHandler: (id: string, error?: any) => string;
-    subscription: Subscription;
+  id: string;
+  instance$: BehaviorSubject<T>;
+  loaded$: BehaviorSubject<boolean>;
+  loading$: BehaviorSubject<boolean>;
+  saving$: BehaviorSubject<boolean>;
+  saved$: BehaviorSubject<boolean>;
+  hasError$: BehaviorSubject<boolean>;
+  error$: BehaviorSubject<string>;
+  genericError: string
+  construct?: () => Observable<T>;
+  persist?: (val: T) => Observable<any>;
+  saved?: (val: any) => void;
+  errorHandler: (id: string, error?: any) => string;
+  subscription: Subscription;
 }
